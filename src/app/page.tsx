da92e133cb6fd5d0a827f8e73a8aed439038e92a
Samuel Nanbam Luka <samuelnanbam047@gmail.com>
2025-08-16 19:38:39 +0100

    add new features/HowItWorks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import HowItWorks from "../components/ui/sections/HowItWorks";
 
 
 export default function Home() {
   return (
    <>
 
-<HowItWorks />
    </>
   );
 }
