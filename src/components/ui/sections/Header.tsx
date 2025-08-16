76fea62fc32bba843a4351be178cbddf84aeb73f
Peter Abahim <lilpero03@gmail.com>
2025-08-16 04:33:20 +0100

    feature, made the nav responsive for mobile

--- src/components/ui/sections/Header.tsx
+++ src/components/ui/sections/Header.tsx
@@ -1,10 +1,15 @@
 'use client'
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { TbMenu } from "react-icons/tb";
+import { TbLogin } from "react-icons/tb";
+import { IoMdClose } from "react-icons/io";
 import Image from 'next/image'
 import logo from "@/public/images/logo.webp"
 import { useState, useRef, useEffect } from 'react';
 
 const Header = () => {
+
+  // FOR THE LANGUAGE DROPDOWN
   const [open, setOpen] = useState(false);
                 const dropdownRef = useRef<HTMLDivElement>(null);
 
@@ -21,15 +26,39 @@ const Header = () => {
                     document.removeEventListener('mousedown', handleClickOutside);
                   };
                 }, [open]);
+      // FOR THE MOBILE AND SIGNIN MENU
+
+      const [IsOpenedMenu, setIsOpenedMenu ] = useState(false)
+
+      const handleIsOpenedMenu = () => {
+        setIsOpenedMenu(!IsOpenedMenu)
+      }
+
+       const [IsOpenedAcc, setIsOpenedAcc ] = useState(false)
+
+      const handleIsOpenedAcc = () => {
+        setIsOpenedAcc(!IsOpenedAcc)
+      }
+
   return (
     <div>
-      <nav className='flex justify-between items-center lg:p-10 lg:px-20'>
+
+      {/* NAV BAR */}
+      <nav className='flex justify-between items-center lg:p-10 lg:px-20 p-5'>
+        
+        {/* MOBILE MENU */}
+        <div onClick={handleIsOpenedMenu} className="lg:hidden text-xl rounded-full p-3 bg-black text-white">
+          {IsOpenedMenu ? <IoMdClose/> : <TbMenu/>}
+        </div>
+
+      {/* LOGO */}
         <div className='flex items-center'>
             <Image className='w-10' src={logo} alt="Logo" />
             <h1 className='text-2xl font-semibold'>PaySlab</h1>
         </div>
 
-        <ul className='flex flex-col lg:flex-row font-medium space-x-10 shadow-xs border border-gray-200 lg:p-3 lg:pt-5 lg:px-10 lg:rounded-full '>
+    {/* NAV NAVIGATION */}
+        <ul className='hidden lg:flex flex-col lg:flex-row font-medium space-x-10 shadow-xs border border-gray-200 lg:p-3 lg:pt-5 lg:px-10 lg:rounded-full '>
           <li className="cursor-pointer">Personal</li>
           <li className="text-gray-300 cursor-pointer text-center">Business 
           <p className="text-[7px]">Coming soon!</p></li>
@@ -37,7 +66,10 @@ const Header = () => {
           <li className="cursor-pointer">Join waitlist</li>
         </ul>
 
-        <div className='flex items-center gap-5'>
+
+        {/* BUTTONS AND LANGUAGE DROPDOWN */}
+
+        <div className='flex items-center lg:gap-5 gap-2'>
             <div>
                 
 
@@ -49,7 +81,7 @@ const Header = () => {
                     EN <MdOutlineKeyboardArrowDown className='text-xl font-medium'/>
                   </h1>
                   {open && (
-                    <div className="absolute right-0 mt-2 w-fit  rounded-xl shadow z-10 bg-black px-2 py-3 text-white">
+                    <div className="absolute right-0 lg:mt-2 mt-16 w-fit  rounded-xl shadow z-10 bg-black px-2 py-3 text-white">
                       <ul className="">
                         <li className="px-4 py-2 hover:bg-gray-900 rounded-xl cursor-pointer">English</li>
                         <li className="px-4 py-2 hover:bg-gray-900 rounded-xl cursor-pointer">Español</li>
@@ -61,10 +93,17 @@ const Header = () => {
                 
             </div>
 
-            <div className='flex'>
+                {/* BUTTON */}
+            <div className='hidden lg:flex'>
                 <button className='bg-black text-white text-sm rounded-l-full font-medium lg:py-4 lg:px-12 hover:bg-gray-300 hover:text-black'>Sign Up</button>
                 <button className='bg-gray-300 text-black text-sm rounded-full font-medium lg:py-4 lg:px-7 relative lg:right-8 hover:bg-white'>Sign In</button>
             </div>
+              
+              {/* MOBILE LOGIN  */}
+            <div onClick={handleIsOpenedAcc} className='lg:hidden text-xl rounded-full p-3 bg-black text-white'>
+                {IsOpenedAcc ? <IoMdClose/> : <TbLogin />}
+                </div>
+            
         </div>
 
       </nav>
