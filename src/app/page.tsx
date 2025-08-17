41c12650541f1cff87bfaf9a83e078ca355313ee
Peter Abahim <lilpero03@gmail.com>
2025-08-17 15:11:39 +0100

    feature/i just displayed the how it works and testimoial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "../components/ui/sections/Hero";
 import CTA from "../components/ui/sections/CTA";
+import HowItWorks from "../components/ui/sections/HowItWorks";
+import Testimonials from "../components/ui/sections/Testimonials";
 export default function Home() {
   return (
    <>
       <Hero />
+      <HowItWorks />
+      <Testimonials />
       <CTA />
    </>
   );
